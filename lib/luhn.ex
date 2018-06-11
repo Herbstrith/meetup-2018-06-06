a60f6323e4066d141c96a0c9f7e2430970322316
Herbstrith <viniherbstrith@gmail.com>
2018-06-10 23:04:50 -0300

    generate accepts integer as input
    Extra unit tests with integer values and invalid float values
    Returning error map when input string is invalid

--- lib/luhn.ex
+++ lib/luhn.ex
@@ -1,14 +1,21 @@
 defmodule Luhn do
 
-  def generate(number) do
+  def generate(number) when is_bitstring(number) do
     unless check_number(number) do
-      false
+      {:error, "input is not a integer"}
     else
       digitsList = String.to_charlist(number) |> Enum.map(&cast_to_number/1)
       doublesSum = double_digits_sum(digitsList)
       singlesSum = single_digits_sum(digitsList)
       rem((doublesSum + singlesSum) * 9, 10)
     end
+  end
+
+  def generate(number) when is_integer(number) do
+    digitsList = Integer.to_charlist(number) |> Enum.map(&cast_to_number/1)
+    doublesSum = double_digits_sum(digitsList)
+    singlesSum = single_digits_sum(digitsList)
+    rem((doublesSum + singlesSum) * 9, 10)
   end
 
   def check_number(number) when is_bitstring(number) do
--- test/luhn_test.exs
+++ test/luhn_test.exs
@@ -20,11 +20,30 @@ defmodule LuhnTest do
     assert Luhn.sum_digits(15) == 6
   end
 
-  test "calculate the luhn digit" do
+  test "calculate the luhn digit with string as input" do
     assert Luhn.generate("7992739871") == 3
     assert Luhn.generate("7992739875") == 5
     assert Luhn.generate("1234567890") == 3
     assert Luhn.generate("565487") == 6
     assert Luhn.generate("5654871") == 2
   end
+
+  test "calculate the luhn digit with an integer as input" do
+    assert Luhn.generate(7992739871) == 3
+    assert Luhn.generate(7992739875) == 5
+    assert Luhn.generate(1234567890) == 3
+    assert Luhn.generate(565487) == 6
+    assert Luhn.generate(5654871) == 2
+  end
+
+  test "Should not generate luhn digit for invalid string input (float)" do
+    assert Luhn.generate("72132.1") === {:error, "input is not a integer"}
+  end
+
+  test "Should not generate luhn digit for invalid input (float)" do
+    assert_raise FunctionClauseError, fn ->
+      Luhn.generate(72132.1)
+    end
+  end
+
 end
