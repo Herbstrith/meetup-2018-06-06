7a178bec0bfa23df1bd5758528a6fc659563aa2e
Herbstrith <viniherbstrith@gmail.com>
2018-06-10 23:26:17 -0300

    Refactor luhn generate and unit tests
    Made methods private
    Removed unit tests which used private methods
    renamed generate to generate_check_digit and extracted duplicated code
    to its own method

--- lib/luhn.ex
+++ lib/luhn.ex
@@ -1,28 +1,28 @@
 defmodule Luhn do
-
-  def generate(number) when is_bitstring(number) do
+  def generate_check_digit(number) when is_bitstring(number) do
     unless check_number(number) do
       {:error, "input is not a integer"}
     else
-      digitsList = String.to_charlist(number) |> Enum.map(&cast_to_number/1)
-      doublesSum = double_digits_sum(digitsList)
-      singlesSum = single_digits_sum(digitsList)
-      rem((doublesSum + singlesSum) * 9, 10)
+      generate(String.to_charlist(number))
     end
   end
 
-  def generate(number) when is_integer(number) do
-    digitsList = Integer.to_charlist(number) |> Enum.map(&cast_to_number/1)
+  def generate_check_digit(number) when is_integer(number) do
+    generate(Integer.to_charlist(number))
+  end
+
+  def generate(number) do
+    digitsList = Enum.map(number, &cast_to_number/1)
     doublesSum = double_digits_sum(digitsList)
     singlesSum = single_digits_sum(digitsList)
     rem((doublesSum + singlesSum) * 9, 10)
   end
 
-  def check_number(number) when is_bitstring(number) do
+  defp check_number(number) when is_bitstring(number) do
     Regex.match?(~r/^\d*$/, number)
   end
 
-  def double_digits_sum(number) do
+  defp double_digits_sum(number) do
     listToIterate = cond do
       rem(length(number) + 1, 2) != 0 -> tl(number)
       true -> number
@@ -33,7 +33,7 @@ defmodule Luhn do
       |> Enum.sum()
   end
 
-  def single_digits_sum(number) do
+  defp single_digits_sum(number) do
     listToIterate = cond do
       rem(length(number) + 1, 2) != 0 -> number
       true -> number
@@ -42,15 +42,15 @@ defmodule Luhn do
       |> Enum.sum()
   end
 
-  def cast_to_number(string) do
+  defp cast_to_number(string) do
     string - ?0
   end
 
-  def sum_digits(number) when number > 10 do
+  defp sum_digits(number) when number > 10 do
     rem(number, 10) + div(number, 10)
   end
 
-  def sum_digits(number), do: number
+  defp sum_digits(number), do: number
 
   defp double(x), do: 2 * x
 
--- test/luhn_test.exs
+++ test/luhn_test.exs
@@ -2,48 +2,38 @@ defmodule LuhnTest do
   use ExUnit.Case
   doctest Luhn
 
-  test "invalid digits" do
-    assert Luhn.check_number("79a27398713") == false
+  test "Should calculate the luhn digit with string as input" do
+    assert Luhn.generate_check_digit("7992739871") == 3
+    assert Luhn.generate_check_digit("7992739875") == 5
+    assert Luhn.generate_check_digit("1234567890") == 3
+    assert Luhn.generate_check_digit("565487") == 6
+    assert Luhn.generate_check_digit("5654871") == 2
   end
 
-  test "invalid input string with a space" do
-    assert Luhn.check_number("792739871 3") == false
-  end
-
-  test "valid digits" do
-    assert Luhn.check_number("79427398713") == true
-  end
-
-  test "sum_digits" do
-    assert Luhn.sum_digits(12) == 3
-    assert Luhn.sum_digits(13) == 4
-    assert Luhn.sum_digits(15) == 6
-  end
-
-  test "calculate the luhn digit with string as input" do
-    assert Luhn.generate("7992739871") == 3
-    assert Luhn.generate("7992739875") == 5
-    assert Luhn.generate("1234567890") == 3
-    assert Luhn.generate("565487") == 6
-    assert Luhn.generate("5654871") == 2
-  end
-
-  test "calculate the luhn digit with an integer as input" do
-    assert Luhn.generate(7992739871) == 3
-    assert Luhn.generate(7992739875) == 5
-    assert Luhn.generate(1234567890) == 3
-    assert Luhn.generate(565487) == 6
-    assert Luhn.generate(5654871) == 2
+  test "Should calculate the luhn digit with an integer as input" do
+    assert Luhn.generate_check_digit(7992739871) == 3
+    assert Luhn.generate_check_digit(7992739875) == 5
+    assert Luhn.generate_check_digit(1234567890) == 3
+    assert Luhn.generate_check_digit(565487) == 6
+    assert Luhn.generate_check_digit(5654871) == 2
   end
 
   test "Should not generate luhn digit for invalid string input (float)" do
-    assert Luhn.generate("72132.1") === {:error, "input is not a integer"}
+    assert Luhn.generate_check_digit("72132.1") == {:error, "input is not a integer"}
   end
 
   test "Should not generate luhn digit for invalid input (float)" do
     assert_raise FunctionClauseError, fn ->
-      Luhn.generate(72132.1)
+      Luhn.generate_check_digit(72132.1)
     end
+  end
+
+  test "Should not generate luhn digit for invalid string input (contains letters)" do
+    assert Luhn.generate_check_digit("79a27398713") == {:error, "input is not a integer"}
+  end
+
+  test "Should not generate luhn digit for invalid string input (contains space)" do
+    assert Luhn.generate_check_digit("792739871 3") == {:error, "input is not a integer"}
   end
 
 end
